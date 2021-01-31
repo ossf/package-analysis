3e0e84ce4e447ce5d8ce753bae0576a58d8dafba
Dan Lorenc <dlorenc@google.com>
2021-01-30 18:42:19 -0600

    Create pods!

--- scheduler/go.mod
+++ scheduler/go.mod
@@ -2,4 +2,10 @@ module github.com/ossf/package-feeds/scheduler
 
 go 1.15
 
-require gocloud.dev v0.21.0
+require (
+	github.com/jordan-wright/ossmalware v0.0.0-20201130035417-0b6db94c48ec
+	gocloud.dev v0.21.0
+	k8s.io/api v0.18.12
+	k8s.io/apimachinery v0.18.12
+	k8s.io/client-go v0.18.12
+)
--- scheduler/main.go
+++ scheduler/main.go
@@ -2,14 +2,34 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"log"
 	"os"
 
+	"github.com/jordan-wright/ossmalware/pkg/library"
 	"gocloud.dev/pubsub"
 	_ "gocloud.dev/pubsub/gcppubsub"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
 
+var clientset *kubernetes.Clientset
+
 func main() {
+
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+	// creates the clientset
+	clientset, err = kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
 
 	subscriptionUrl := os.Getenv("OSSMALWARE_SUBSCRIPTION_URL")
 	ctx := context.Background()
@@ -25,8 +45,64 @@ func main() {
 			continue
 		}
 		go func(m *pubsub.Message) {
-			defer m.Ack()
 			log.Println("handling message: ", string(m.Body))
+			pkg := library.Package{}
+			if err := json.Unmarshal(m.Body, &pkg); err != nil {
+				log.Println("error unmarshalling json: ", err)
+				return
+			}
+			if err := handlePkg(pkg); err != nil {
+				fmt.Println("Error: ", err)
+				msg.Nack()
+				return
+			}
+			msg.Ack()
 		}(msg)
 	}
+}
+
+func handlePkg(pkg library.Package) error {
+	// Turn it into a Pod!
+	switch pkg.Type {
+	case "pypi":
+		return createPypiPod(pkg.Name, pkg.Version)
+	case "npm":
+		// createNpmPod(pkg.Name, pkg.Version)
+	}
+	return nil
+}
+
+func createPypiPod(name, version string) error {
+	pods := clientset.CoreV1().Pods("default")
+	pod, err := pods.Create(context.Background(), &v1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: fmt.Sprintf("%s", name),
+			Labels: map[string]string{
+				"install": "1",
+			},
+		},
+		Spec: v1.PodSpec{
+			RestartPolicy: v1.RestartPolicyNever,
+			Containers: []v1.Container{
+				{
+					Name:    "install",
+					Image:   "python:3",
+					Command: []string{"/bin/bash", "-c"},
+					Args: []string{
+						fmt.Sprintf("mkdir -p /app && cd /app && pip3 install %s==%s", name, version),
+					},
+					Resources: v1.ResourceRequirements{
+						Requests: v1.ResourceList{
+							v1.ResourceCPU: resource.MustParse("250m"),
+						},
+					},
+				},
+			},
+		},
+	}, metav1.CreateOptions{})
+	if err != nil {
+		return err
+	}
+	fmt.Println("Created pod: ", pod.Name)
+	return nil
 }
