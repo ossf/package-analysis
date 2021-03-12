0d1b741e63d41d4dbc5f2f40dc61c1a24c8312e8
Oliver Chang <ochang@google.com>
2021-03-12 17:20:04 +1100

    Fix node and python runner scripts.
    - Set the expected labels and annotations.
    - Use a randomly generated name to avoid invalid characters.

--- analysis/python-runner.sh
+++ analysis/python-runner.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 for pkg in $(cat python.txt); do
-    kubectl run $pkg --image=python:3 --restart='Never' --labels=install=1 --requests="cpu=250m" -- sh -c "mkdir -p /app && cd /app && pip3 install $pkg"
+  rand=$(cat /dev/urandom | tr -dc 'a-z0-9' | head -c 10)
+  kubectl run pypi-$rand --image=python:3 --restart='Never' \
+      --labels=install=1,package_type=pypi \
+      --annotations=package_name=$pkg \
+      --annotations=package_version=test \
+      --requests="cpu=250m" -- sh -c "mkdir -p /app && cd /app && pip3 install $pkg"
 done
