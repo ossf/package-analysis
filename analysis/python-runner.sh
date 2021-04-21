88a29b74ab9322a0a422b0d8980401ccd74a060f
Oliver Chang <ochang@google.com>
2021-04-21 10:08:59 +1000

    comment

--- analysis/python-runner.sh
+++ analysis/python-runner.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+REGISTRY=gcr.io/ossf-malware-analysis
+
 for pkg in $(cat python.txt); do
   rand=$(cat /dev/urandom | tr -dc 'a-z0-9' | head -c 10)
-  kubectl run pypi-$rand --image=gcr.io/ossf-malware-analysis/python --restart='Never' \
+  kubectl run pypi-$rand --image=$REGISTRY/python --restart='Never' \
       --labels=install=1,package_type=pypi \
       --annotations=package_name=$pkg \
       --annotations=package_version=test \
