65ae71ea6eff88075b9b63191ad783f95b91a457
Caleb Brown <calebbrown@google.com>
2021-11-15 11:11:06 +1100

    Relocate and generalize the scripts that inject packages for the worker.

--- analysis/ruby-runner.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-topic=${OSSMALWARE_WORKER_TOPIC:-projects/ossf-malware-analysis/topics/workers}
-for pkg in $(cat rubygems.txt); do
-  gcloud pubsub topics publish "$topic" --attribute=name=$pkg,ecosystem=rubygems
-done
