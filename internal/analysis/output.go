abc13d6fcebeb000053ca24d23133dea3292fe64
Caleb Brown <calebbrown@google.com>
2022-01-24 15:58:01 +1100

    Add a status, stdout and stderr to the results returned for each command.
    The stdout and stderr is limited to the last 20 lines (max 4k bytes)
    from each.
    
    This makes it easier to debug and measure the pipelines quality.

---
+++ internal/analysis/output.go
@@ -0,0 +1,36 @@
+package analysis
+
+// lastLines returns a byte array containing only the last maxLines of lines
+// in b.
+//
+// A line is defined the by the occurance of a '\n' in the output.
+//
+// If there are fewer than maxLines then the entire intput byte array will be
+// returned.
+//
+// No more than maxBytes will be in the returned byte array.
+func lastLines(b []byte, maxLines, maxBytes int) []byte {
+	if len(b) == 0 {
+		return b
+	}
+	// only consider the last maxBytes of b, or all of b if maxBytes is bigger
+	// than the size of b.
+	startOff := len(b) - maxBytes
+	if startOff < 0 {
+		startOff = 0
+	}
+	b = b[startOff:]
+	// count each newline from the end of the bufffer
+	lines := 0
+	lineOff := len(b) - 1
+	for off := len(b) - 1; off > 0; off-- {
+		if b[off] == '\n' {
+			lines++
+			lineOff = off
+		}
+		if lines > maxLines {
+			return b[lineOff+1:]
+		}
+	}
+	return b
+}
