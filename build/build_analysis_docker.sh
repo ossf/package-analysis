213e7871e63bcd7b72aa80bb7dcadb43c8ff121b
Max Fisher <maxfisher@google.com>
2022-09-14 12:08:13 +1000

    analysis: add helper scripts
    Signed-off-by: Max Fisher <maxfisher@google.com>

---
+++ build/build_analysis_docker.sh
@@ -0,0 +1,11 @@
+
+#!/bin/bash -ex
+
+# Rebuilds the local analysis image only
+
+BASE_PATH="$(dirname $(dirname $(realpath $0)))"
+REGISTRY=gcr.io/ossf-malware-analysis
+
+pushd "$BASE_PATH"
+docker build -t $REGISTRY/analysis -f cmd/analyze/Dockerfile .
+popd
