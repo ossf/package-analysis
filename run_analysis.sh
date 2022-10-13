d9035f942527b3bb5a4a7ea8a26d255d245707cc
Max Fisher <112151114+maxfisher-g@users.noreply.github.com>
2022-10-13 12:58:18 +1100

    Improve run_analysis.sh script with local package functionality (#391)
    * run_analysis.sh: allow passing a path to run analysis of a local package
    
    Signed-off-by: Max Fisher <maxfisher@google.com>
    
    * run_analysis.sh: print delimit script termination from docker output
    
    Signed-off-by: Max Fisher <maxfisher@google.com>
    
    * run_analysis.sh: Print out package name again at end of script
    
    Signed-off-by: Max Fisher <maxfisher@google.com>
    
    Signed-off-by: Max Fisher <maxfisher@google.com>

--- run_analysis.sh
+++ run_analysis.sh
@@ -2,36 +2,68 @@
 
 ECOSYSTEM="$1"
 PACKAGE="$2"
+PKG_PATH="$3"
 
-if [[ $# != 2 ]]; then
-	echo "Usage: $0 (npm|packagist|pypi|rubygems) <package name>"
-	exit -1
+if [[ $# != 2 && $# != 3 ]]; then
+	echo "Usage: $0 (npm|packagist|pypi|rubygems) <package name> [/path/to/local/package.file]"
+	exit 255
 fi
 
 RESULTS_DIR="/tmp/results"
 LOGS_DIR="/tmp/dockertmp"
 
+LINE="-----------------------------------------"
+
 mkdir -p "$RESULTS_DIR"
 mkdir -p "$LOGS_DIR"
 
-docker run --cgroupns=host --privileged -ti \
-    -v "$RESULTS_DIR":/results \
-    -v "$LOGS_DIR":/tmp \
-    -v /var/lib/containers:/var/lib/containers \
-    gcr.io/ossf-malware-analysis/analysis analyze \
-    -package "$PACKAGE" -ecosystem "$ECOSYSTEM" \
-    -upload file:///results/
 
-EXIT_CODE=$?
+DOCKER_OPTS=(run --cgroupns=host --privileged -ti)
+
+DOCKER_MOUNTS=(-v /var/lib/containers:/var/lib/containers -v "$RESULTS_DIR":/results -v "$LOGS_DIR":/tmp)
+
+ANALYSIS_IMAGE=gcr.io/ossf-malware-analysis/analysis
+
+ANALYSIS_ARGS=(analyze -package "$PACKAGE" -ecosystem "$ECOSYSTEM" -upload file:///results/)
+
+echo $LINE
+
+if [[ -n "$PKG_PATH" ]]; then 
+	# local mode
+	PKG_FILE=$(basename "$PKG_PATH")
+	echo "Analysing $ECOSYSTEM package $PACKAGE from local file $PKG_FILE"
+	echo "Path: $PKG_PATH"
+
+	# mount local package file in root of docker image
+	DOCKER_MOUNTS+=(-v "$PKG_PATH:/$PKG_FILE")
+
+	# tell analyis to use mounted package file
+	ANALYSIS_ARGS+=(-local "/$PKG_FILE")
+else
+	# remote mode
+	echo "Analysing $ECOSYSTEM package $PACKAGE (remote)"
+fi
+
+echo $LINE
+echo
+
+sleep 1
+
+docker ${DOCKER_OPTS[@]} ${DOCKER_MOUNTS[@]} $ANALYSIS_IMAGE ${ANALYSIS_ARGS[@]}
+DOCKER_EXIT_CODE=$?
 
 echo
-if [[ $EXIT_CODE == 0 ]]; then
-	echo "Finished"
+
+echo $LINE
+if [[ $DOCKER_EXIT_CODE == 0 ]]; then
+	echo "Finished analysis of $ECOSYSTEM package $PACKAGE"
 	echo "Results dir: $RESULTS_DIR"
 	echo "Logs dir: $LOGS_DIR"
 else
-	echo "Docker process exited with nonzero exit code $EXIT_CODE"
+	echo "Failed to analyse $ECOSYSTEM package $PACKAGE"
+	echo "Docker process exited with nonzero exit code $DOCKER_EXIT_CODE"
 	rmdir --ignore-fail-on-non-empty "$RESULTS_DIR"
 	rmdir --ignore-fail-on-non-empty "$LOGS_DIR"
-	exit $EXIT_CODE
+	exit $DOCKER_EXIT_CODE
 fi
+echo $LINE
