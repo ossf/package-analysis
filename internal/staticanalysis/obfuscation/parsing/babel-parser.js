11c77de00c73b884fb60ed1a0a132c2bb0d3400a
Max Fisher <maxfisher@google.com>
2022-11-03 12:10:27 +1100

    rewrite babel parser to use babel traverse
    Signed-off-by: Max Fisher <maxfisher@google.com>

--- internal/staticanalysis/obfuscation/parsing/babel-parser.js
+++ internal/staticanalysis/obfuscation/parsing/babel-parser.js
@@ -3,9 +3,13 @@
 // jshint esversion: 6
 "use strict";
 
-const parser = require("@babel/parser");
-const traverse = require("@babel/traverse");
-const fs = require("fs");
+import fs from "fs";
+import parser from "@babel/parser";
+
+// See https://github.com/babel/babel/issues/13855
+import _traverse from "@babel/traverse";
+const traverse = _traverse.default;
+
 
 function locationString(node) {
     return (node.loc !== null) ? `[${node.loc.start.line},${node.loc.start.column}]` : "[]";
@@ -20,6 +24,7 @@ function logJSON(type, subtype, name, pos, array, extra = null) {
         `"array":${arrayValue}, "extra":${extraValue}}`;
     parseOutputLines.push(json);
 }
+
 function logIdentifierJSON(subtype, name, pos, extra = null) {
     logJSON("Identifier", subtype, name, pos, null, extra);
 }
@@ -28,241 +33,127 @@ function logLiteralJSON(subtype, value, pos, inArray, extra = null) {
     logJSON("Literal", subtype, value, pos, inArray, extra);
 }
 
+function logParameter(p) {
+    if (p === null) {
+        return;
+    }
+    if (p.type === "Identifier") {
+        // simple function parameter name: function f(x)
+        logIdentifierJSON("Parameter", p.name, locationString(p));
+    } else if (p.type === "AssignmentPattern" && p.left.type === "Identifier") {
+        // parameter with default value: function f(x = 3)
+        logIdentifierJSON("Parameter", p.left.name, locationString(p));
+    }
+}
+
 function logParameters(node) {
     for (let p of node.params) {
-        switch (p.type) {
-            // simple function parameter name:
-            // function f(x)
-            case "Identifier":
-                logIdentifierJSON("Parameter", p.name, locationString(p));
-                break;
-            // parameter with default value:
-            // function f(x = 3)
-            case "AssignmentPattern":
-                if (p.left.type === "Identifier") {
-                    logIdentifierJSON("Parameter", p.left.name, locationString(p));
-                } else {
-                    // This is too hard...
-                    walkAst(p.left);
-                }
-                walkAst(p.right);
-                break;
+        logParameter(p);
+    }
+}
+
+function traverseAst(ast, printDebug = false) {
+    const state = { isInArray: false };
+
+    const arrayVisitor = {
+        StringLiteral: function(path) {
+            const loc = locationString(path.node);
+            logLiteralJSON("String", path.node.value, loc, true, path.node.extra);
+        },
+        NumericLiteral: function(path) {
+            const loc = locationString(path.node);
+            logLiteralJSON("Numeric", path.node.value, loc, true, path.node.extra);
+        },
+        TemplateElement: function(path) {
+            const loc = locationString(path.node);
+            logLiteralJSON("StringTemplate", path.node.value.raw, loc, true, path.node.value);
         }
-    }
-}
+    };
 
-function multiWalkAst(nodeList, isInArray = false) {
-    if (nodeList === null) {
-        return;
-    }
-    for (let n of nodeList) {
-        walkAst(n, isInArray);
-    }
-}
-
-function walkAst(startNode, isInArray = false, printDebug = false) {
-    // walk the AST and print out any literals
-    if (startNode === null) {
-        return;
-    }
-    const n = startNode;
-
-    if (printDebug) {
-        console.log(`# type: ${n.type}`);
-    }
-
-    const loc = locationString(n);
-    switch (n.type) {
-        case "File":
-            walkAst(n.program);
-            break;
-        case "Program":
-        // fall-through
-        case "BlockStatement":
-            multiWalkAst(n.body, isInArray);
-            break;
-        case "ArrowFunctionExpression":
-        // fall-through
-        case "FunctionDeclaration":
-            if (n.id !== null) {
-                logIdentifierJSON("Function", n.id.name, locationString(n.id));
+    const astVisitor = {
+        FunctionDeclaration: function (path) {
+            if (path.node.id !== null) {
+                logIdentifierJSON("Function", path.node.id.name, locationString(path.node.id));
             }
-            logParameters(n);
-            walkAst(n.body);
-            break;
-        case "AwaitExpression":
-            walkAst(n.argument);
-            break;
-        case "AssignmentExpression":
-            if (n.left.type === "Identifier") {
-                logIdentifierJSON("AssignmentTarget", n.left.name, locationString(n.left));
-            } else {
-                walkAst(n.left);
+            logParameters(path.node);
+        },
+        AssignmentExpression: function (path) {
+            if (path.node.left.type === "Identifier") {
+                logIdentifierJSON("AssignmentTarget", path.node.left.name, locationString(path.node.left));
             }
-            walkAst(n.right);
-            break;
-        case "BinaryExpression":
-            walkAst(n.left);
-            walkAst(n.right);
-            break;
-        case "ExpressionStatement":
-            walkAst(n.expression);
-            break;
-        case "CallExpression":
-            walkAst(n.callee);
-            multiWalkAst(n.arguments, isInArray);
-            break;
-        case "ClassExpression":
-            if (n.id !== null) {
-                logIdentifierJSON("Class", n.id.name, locationString(n.id));
+        },
+        ClassExpression: function (path) {
+            if (path.node.id !== null) {
+                logIdentifierJSON("Class", path.node.id.name, locationString(path.node.id));
             }
-            walkAst(n.body);
             // TODO superclass, decorators?
-            break;
-        case "ClassBody":
-            multiWalkAst(n.body);
-            break;
-        case "ClassMethod":
-            if (n.kind !== "Constructor" && n.key.type === "Identifier") {
-                logIdentifierJSON("Method", n.key.name, locationString(n.key));
+        },
+        ClassMethod: function (path) {
+            if (path.node.kind !== "Constructor" && path.node.key.type === "Identifier") {
+                logIdentifierJSON("Method", path.node.key.name, locationString(path.node.key));
             }
-            logParameters(n);
-            walkAst(n.body);
-            break;
-        case "ClassPrivateMethod":
-            // key is PrivateName
-            logIdentifierJSON("Method", n.key.id.name, locationString(n.key.id));
-            logParameters(n);
-            walkAst(n.body);
-            break;
-        case "ClassProperty":
-            if (n.key.type === "Identifier") {
-                logIdentifierJSON("Property", n.key.name, locationString(n.key));
-            } else {
-                walkAst(n.key);
+            logParameters(path.node);
+        },
+        ClassPrivateMethod: function (path) {
+            // path.node.key has type PrivateName
+            logIdentifierJSON("Method", path.node.key.id.name, locationString(path.node.key.id));
+            logParameters(path.node);
+        },
+        ClassProperty: function (path) {
+            if (path.node.key.type === "Identifier") {
+                logIdentifierJSON("Property", path.node.key.name, locationString(path.node.key));
             }
-            walkAst(n.value);
-            break;
-        case "ClassPrivateProperty":
-            // key is PrivateName
-            logIdentifierJSON("Property", n.key.id.name, locationString(n.key.id));
-            walkAst(n.value);
-            break;
-        case "StaticBlock":
-            multiWalkAst(n.body);
-            break;
-        case "MemberExpression":
-            walkAst(n.object);
+        },
+        ClassPrivateProperty: function (path) {
+            // path.node.key is PrivateName
+            logIdentifierJSON("Property", path.node.key.id.name, locationString(path.node.key.id));
+        },
+        MemberExpression: function (path) {
             // Should be either Identifier or PrivateName if static (a.b) property
             // else Expression if computed (a[b]) property
-            switch (n.property.type) {
-                case "Identifier":
-                    logIdentifierJSON("Member", n.property.name, locationString(n.property));
-                    break;
-                case "PrivateName":
-                    logIdentifierJSON("Member", n.property.id.name, locationString(n.property.id));
-                    break;
-                default:
-                    if (!n.computed && printDebug) {
-                        console.log(`Warning: MemberExpression had property of type ${n.property.type} but was not computed`);
-                        console.log(n.property);
-                    }
-                    walkAst(n.property);
-                    break;
+            let identifier = null;
+            if (path.node.property.type === "Identifier") {
+                identifier = path.node.property;
+            } else if (path.node.property.type === "PrivateName") {
+                identifier = path.node.property.id;
             }
-            break;
-        case "ThisExpression":
-            // nothing to do
-            break;
-        case "ReturnStatement":
-            walkAst(n.argument);
-            break;
-        case "LabeledStatement":
-            logIdentifierJSON("StatementLabel", n.label.name, locationString(n.label));
-            walkAst(n.body);
-            break;
-        case "BreakStatement":
-            // fall-through
-        case "ContinueStatement":
-            // the only thing of interest would be the label, which must already have been defined somewhere else
-            break;
-        case "WhileStatement":
-            // fall-through
-        case "DoWhileStatement":
-            walkAst(n.test);
-            walkAst(n.body);
-            break;
-        case "ForStatement":
-            walkAst(n.init);
-            walkAst(n.test);
-            walkAst(n.update);
-            walkAst(n.body);
-            break;
-        case "ForOfStatement":
-            // fall-through
-        case "ForInStatement":
-            walkAst(n.left);
-            walkAst(n.right);
-            walkAst(n.body);
-            break;
-        case "TryStatement":
-            walkAst(n.block);
-            walkAst(n.handler);
-            walkAst(n.finalizer);
-            break;
-        case "ThrowStatement":
-            walkAst(n.argument);
-            break;
-        case "CatchClause":
-            walkAst(n.param);
-            walkAst(n.body);
-            break;
-        case "IfStatement":
-            walkAst(n.test);
-            walkAst(n.consequent);
-            walkAst(n.alternate);
-            break;
-        case "SwitchStatement":
-            walkAst(n.discriminant);
-            multiWalkAst(n.cases);
-            break;
-        case "SwitchCase":
-            walkAst(n.test);
-            multiWalkAst(n.consequent);
-            break;
-        case "VariableDeclaration":
-            multiWalkAst(n.declarations, isInArray);
-            break;
-        case "VariableDeclarator":
-            logIdentifierJSON("Variable", n.id.name, locationString(n.id));
-            walkAst(n.init);
-            break;
-        case "Identifier":
-            logIdentifierJSON("Other", n.name, loc);
-            break;
-        case "StringLiteral":
-            logLiteralJSON("String", n.value, loc, isInArray, n.extra);
-            break;
-        case "NumericLiteral":
-            logLiteralJSON("Numeric", n.value, loc, isInArray, n.extra);
-            break;
-        case "ArrayExpression":
-            multiWalkAst(n.elements, true);
-            break;
-        case "TemplateLiteral":
-            multiWalkAst(n.quasis, isInArray);
-            multiWalkAst(n.expressions, isInArray);
-            break;
-        case "TemplateElement":
-            logLiteralJSON("StringTemplate", n.value.raw, loc, isInArray, n.value);
-            break;
-        default:
-            if (printDebug) {
-                console.log(`Found unknown node of type ${n.type} node @ ${loc}`);
-                console.log(n);
+            if (identifier !== null) {
+                logIdentifierJSON("Member", identifier.name, locationString(identifier));
             }
-    }
+        },
+        CatchClause: function(path) {
+            logParameter(path.node.param);
+        },
+        LabeledStatement: function (path) {
+            logIdentifierJSON("StatementLabel", path.node.label.name, locationString(path.node.label));
+        },
+        VariableDeclarator: function (path) {
+            logIdentifierJSON("Variable", path.node.id.name, locationString(path.node.id));
+        },
+        //Identifier: function (path) {
+        //    const loc = locationString(path.node);
+        //    logIdentifierJSON("Unrecognised", path.node.name, loc);
+        //},
+        StringLiteral: function (path) {
+            const loc = locationString(path.node);
+            logLiteralJSON("String", path.node.value, loc, false, path.node.extra);
+        },
+        NumericLiteral: function (path) {
+            const loc = locationString(path.node);
+            logLiteralJSON("Numeric", path.node.value, loc, false, path.node.extra);
+        },
+        ArrayExpression: function (path) {
+            path.traverse(arrayVisitor);
+            path.skip();
+        },
+        TemplateElement: function (path) {
+            const loc = locationString(path.node);
+            logLiteralJSON("StringTemplate", path.node.value.raw, loc, false, path.node.value);
+        }
+    };
+    traverse(ast, astVisitor, state);
 }
+
 
 function findLiteralsAndIdentifiers(source, printDebug) {
     const ast = parser.parse(source);
@@ -272,7 +163,7 @@ function findLiteralsAndIdentifiers(source, printDebug) {
         console.log(JSON.stringify(ast, null, "  "));
     }
 
-    walkAst(ast, false, printDebug);
+    traverseAst(ast);
 
     const allJson = "[\n" + parseOutputLines.join(",\n") + "\n]";
     console.log(allJson);
@@ -285,7 +176,7 @@ function main() {
     // Therefore running this in a terminal in interactive mode with no file piped into stdin will
     // cause the read to fail with EAGAIN
     // Passing the raw '0' as the fd avoids this issue.
-    const sourceFile = process.argv.length >= 3 ? process.argv[2] : 0; // process.stdin.fd;
+    const sourceFile = process.argv.length >= 3 ? process.argv[2] : 0;
     const sourceCode = fs.readFileSync(sourceFile, "utf8");
     if (printDebug) {
         console.log("Read source:");
