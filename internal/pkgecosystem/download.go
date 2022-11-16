addccb6a9459fafb438926265fddcc3ad6f826bf
Max Fisher <maxfisher@google.com>
2022-11-16 18:04:32 +1100

    address review comments
    Signed-off-by: Max Fisher <maxfisher@google.com>

--- internal/pkgecosystem/download.go
+++ internal/pkgecosystem/download.go
@@ -5,14 +5,8 @@ import (
 	"io"
 	"net/http"
 	"os"
-	"strings"
+	"path"
 )
-
-// getLastUrlElement returns the last element in a (URL) string split on the '/' character
-func getLastUrlElement(url string) string {
-	elements := strings.Split(url, "/")
-	return elements[len(elements)-1]
-}
 
 /*
 downloadToDirectory downloads a file from the given URL to the given directory. The name of the
@@ -20,9 +14,8 @@ local file is obtained from the last element of the URL path when split on the '
 On successful download, the full path to the downloaded file is returned.
 */
 func downloadToDirectory(dir string, url string) (string, error) {
-	fileName := getLastUrlElement(url)
+	fileName := path.Base(url)
 	filePath := dir + string(os.PathSeparator) + fileName
-
 	err := downloadToPath(filePath, url)
 
 	if err != nil {
@@ -37,22 +30,20 @@ downloadToPath creates (and/or truncates) a file at the given path, then writes
 contents of whatever is at the given URL to that given file using downloadToFile,
 and finally closes the file.
 */
-func downloadToPath(path string, url string) error {
+func downloadToPath(path string, url string) (err error) {
 	file, err := os.Create(path)
 	if err != nil {
 		return err
 	}
 
-	closeFile := func() {
+	defer func() {
 		closeErr := file.Close()
 		if err == nil {
 			err = closeErr
 		} else if closeErr != nil {
 			err = fmt.Errorf("%v; error closing file %s: %v", err, path, closeErr)
 		}
-	}
-
-	defer closeFile()
+	}()
 
 	err = downloadToFile(file, url)
 
