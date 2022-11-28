d3f3fa18fd2fd70ea198c8516b65fc8529215399
Max Fisher <maxfisher@google.com>
2022-11-29 01:06:21 +1100

    write_file.go: remove stat and simplify greatly
    Signed-off-by: Max Fisher <maxfisher@google.com>

--- internal/utils/write_file.go
+++ internal/utils/write_file.go
@@ -1,45 +1,24 @@
 package utils
 
 import (
-	"errors"
 	"fmt"
-	"io/fs"
 	"os"
 )
-
-const defaultFileMode os.FileMode = 0666
-const executableFlag os.FileMode = 0111
 
 /*
 WriteFile writes the given file contents to the given path.
 The file may optionally be marked as executable.
-
-In more detail:
-
- 1. If a non-regular file exists at path, an error is returned.
- 2. If a regular file exists at path, it is truncated and marked as executable if applicable.
- 3. If no file exists at path, a new one is created with executable permissions if applicable
-
-In cases 2 and 3 above, the contents of scriptSource are then written to the file.
 */
-func WriteFile(path string, contents []byte, executable bool) (err error) {
-	fileInfo, err := os.Stat(path)
-
-	if err != nil && !errors.Is(err, fs.ErrNotExist) {
+func WriteFile(path string, contents []byte, executable bool) error {
+	if err := os.WriteFile(path, contents, 0666); err != nil {
 		return err
 	}
 
-	if err == nil && executable {
-		// file exists - make sure it's executable
-		if err := os.Chmod(path, fileInfo.Mode().Perm()|executableFlag); err != nil {
-			return fmt.Errorf("could not set executable bit on %s: %v", path, err)
+	if executable {
+		if err := os.Chmod(path, 0777); err != nil {
+			return fmt.Errorf("could not set exec permissions on %s: %v", path, err)
 		}
 	}
 
-	fileMode := defaultFileMode
-	if executable {
-		fileMode |= executableFlag
-	}
-
-	return os.WriteFile(path, contents, fileMode)
+	return nil
 }
