b2f0bd2b2558cfa0579afa52b279eb64d1aab2e1
Max Fisher <maxfisher@google.com>
2023-01-04 19:34:16 +1100

    fix bug in base64 detection
    Signed-off-by: Max Fisher <maxfisher@google.com>

--- internal/staticanalysis/obfuscation/detections/base64.go
+++ internal/staticanalysis/obfuscation/detections/base64.go
@@ -68,7 +68,7 @@ func FindBase64Substrings(s string) []string {
 	matches := []string{}
 
 	for _, candidate := range base64Regex.FindAllString(s, -1) {
-		if looksLikeActualBase64(s) {
+		if looksLikeActualBase64(candidate) {
 			matches = append(matches, candidate)
 		}
 	}
--- internal/staticanalysis/obfuscation/detections/base64_test.go
+++ internal/staticanalysis/obfuscation/detections/base64_test.go
@@ -31,6 +31,8 @@ func TestFindBase64Substrings(t *testing.T) {
 		{"long base64 string", longBase64String, []string{longBase64String}},
 		{"multiple base64 strings", longBase64String + " " + longBase64String,
 			[]string{longBase64String, longBase64String}},
+		{"multiple base64 strings 2", longBase64String + "!!!!====!!" + longBase64String,
+			[]string{longBase64String, longBase64String}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
