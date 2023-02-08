a88a3ff31127f1b718c8b2058d408efebdfb6c43
Max Fisher <112151114+maxfisher-g@users.noreply.github.com>
2023-02-08 11:13:01 +1100

    Parsing restructure and improvements (#624)
    * restructure babel parser to allow for future batch operation; process errors and info from parser output
    
    Signed-off-by: Max Fisher <maxfisher@google.com>
    
    * rename symbol to token in parsing package and make separate JSON data structures for tokens and status
    
    Signed-off-by: Max Fisher <maxfisher@google.com>
    
    ---------
    
    Signed-off-by: Max Fisher <maxfisher@google.com>

--- internal/staticanalysis/parsing/babel-parser.js
+++ internal/staticanalysis/parsing/babel-parser.js
@@ -11,75 +11,79 @@ import _traverse from "@babel/traverse";
 
 const traverse = _traverse.default;
 
-// If the parser encounters an unrecoverable syntax error (which may indicate
-// that the input is not JavaScript, the program will exit with this exit code.
-const SYNTAX_ERROR_EXIT_CODE = 33;
+// used to signal to parent process that parsing could not complete due to syntax errors
+const fatalSyntaxErrorMarker = "FATAL SYNTAX ERROR";
 
-function locationString(node) {
-    return (node.loc !== null) ? `[${node.loc.start.line},${node.loc.start.column}]` : "[]";
+function position(node) {
+    return (node.loc !== null) ? [node.loc.start.line,node.loc.start.column] : [];
 }
 
-const parseOutputLines = [];
+// Holds all parsing data for a single file
+class ParseData {
+    constructor() {
+        // holds token information (function, variable names)
+        this.tokens = [];
+        // holds status information (info, errors)
+        this.status = [];
+    }
 
-function logJSON(type, subtype, data, pos, extra = null) {
-    const extraValue = (extra !== null) ? `${JSON.stringify(extra)}` : "{}";
-    const json = `{"type":"${type}","subtype":"${subtype}","data":${JSON.stringify(data)},` +
-        `"pos":${pos},"extra":${extraValue}}`;
-    parseOutputLines.push(json);
-}
+    static makeOutputDict(type, subtype, data, pos, extra = null) {
+        return { "type": type, "subtype": subtype, data: data, pos: pos, extra: (extra === null) ? {} : extra };
+    }
 
-function logError(errorType, message, pos) {
-    logJSON("Error", errorType, message, pos);
-}
+    logError(errorType, message, pos) {
+        this.status.push(ParseData.makeOutputDict("Error", errorType, message, pos));
+    }
 
-function logInfo(infoType, message) {
-    logJSON("Info", infoType, message, "[]");
-}
+    logInfo(infoType, message) {
+        this.status.push(ParseData.makeOutputDict("Info", infoType, message, []));
+    }
 
-function logComment(commentType, comment, pos) {
-    logJSON("Comment", commentType, comment, pos);
-}
+    logComment(commentType, comment, pos) {
+        this.tokens.push(ParseData.makeOutputDict("Comment", commentType, comment, pos));
+    }
 
-function logIdentifierOrPrivateName(identifierType, node) {
-    // if node is a PrivateName, the corresponding Identifier can be found as node.id
-    let identifierNode;
-    switch (node.type) {
-        case "Identifier":
-            identifierNode = node;
-            break;
-        case "PrivateName":
-            identifierNode = node.id;
-            break;
-        default:
-            console.log("Error: logIdentifierNodeJSON passed a node of type " + node.type);
+    logIdentifierOrPrivateName(identifierType, node) {
+        // if node is a PrivateName, the corresponding Identifier can be found as node.id
+        let identifierNode;
+        switch (node.type) {
+            case "Identifier":
+                identifierNode = node;
+                break;
+            case "PrivateName":
+                identifierNode = node.id;
+                break;
+            default:
+                console.log("Error: logIdentifierNodeJSON passed a node of type " + node.type);
+                return;
+        }
+
+        let name = identifierNode.name;
+        let pos = position(identifierNode);
+
+        if (identifierNode.name === undefined) {
+            console.log("Error: undefined identifier name at pos " + pos);
+        }
+
+        this.tokens.push(ParseData.makeOutputDict("Identifier", identifierType, name, pos));
+    }
+
+    logLiteral(literalType, value, pos, inArray, extra = null) {
+        if (value === undefined) {
+            console.log("Error: undefined literal value at pos " + pos);
             return;
+        }
+
+        if (extra === null) {
+            extra = {};
+        }
+
+        extra.array = inArray;
+        this.tokens.push(ParseData.makeOutputDict("Literal", literalType, value, pos, extra));
     }
-
-    let name = identifierNode.name;
-    let pos = locationString(identifierNode);
-
-    if (identifierNode.name === undefined) {
-        console.log("Error: undefined identifier name at pos " + pos);
-    }
-
-    logJSON("Identifier", identifierType, name, pos);
 }
 
-function logLiteral(literalType, value, pos, inArray, extra = null) {
-    if (value === undefined) {
-        console.log("Error: undefined literal value at pos " + pos);
-        return;
-    }
-
-    if (extra === null) {
-        extra = {};
-    }
-
-    extra.array = inArray;
-    logJSON("Literal", literalType, value, pos, extra);
-}
-
-function visitIdentifierOrPrivateName(path) {
+function visitIdentifierOrPrivateName(path, parseData) {
     const node = path.node;
     const parentNode = path.parentPath.node;
     const parentParentNode = path.parentPath.parentPath.node;
@@ -87,67 +91,67 @@ function visitIdentifierOrPrivateName(path) {
     switch (parentNode.type) {
         case "ObjectProperty":
             if (node === parentNode.key) {
-                logIdentifierOrPrivateName("Variable", node);
+                parseData.logIdentifierOrPrivateName("Variable", node);
             }
             break;
         case "ArrayPattern":
-            logIdentifierOrPrivateName("Variable", node);
+            parseData.logIdentifierOrPrivateName("Variable", node);
             break;
         case "VariableDeclarator":
             if (node === parentNode.id) {
-                logIdentifierOrPrivateName("Variable", node);
+                parseData.logIdentifierOrPrivateName("Variable", node);
             }
             break;
         case "FunctionDeclaration":
             if (node === parentNode.id) {
-                logIdentifierOrPrivateName("Function", node);
+                parseData.logIdentifierOrPrivateName("Function", node);
             } else {
-                logIdentifierOrPrivateName("Parameter", node);
+                parseData.logIdentifierOrPrivateName("Parameter", node);
             }
             break;
         case "LabeledStatement":
-            logIdentifierOrPrivateName("StatementLabel", node);
+            parseData.logIdentifierOrPrivateName("StatementLabel", node);
             break;
         case "PrivateName":
             // processed already
             break;
         case "MemberExpression":
             if (node === parentNode.property) {
-                logIdentifierOrPrivateName("Member", node);
+                parseData.logIdentifierOrPrivateName("Member", node);
             }
             break;
         case "CatchClause":
-            logIdentifierOrPrivateName("Parameter", node);
+            parseData.logIdentifierOrPrivateName("Parameter", node);
             break;
         case "ClassPrivateMethod":
             // fall through
         case "ClassMethod":
             if (node === parentNode.key) {
                 if (parentNode.kind !== "constructor") {
-                    logIdentifierOrPrivateName("Method", node);
+                    parseData.logIdentifierOrPrivateName("Method", node);
                 }
             } else {
-                logIdentifierOrPrivateName("Parameter", node);
+                parseData.logIdentifierOrPrivateName("Parameter", node);
             }
             break;
         case "ClassPrivateProperty":
             // fall through
         case "ClassProperty":
-            logIdentifierOrPrivateName("Property", node);
+            parseData.logIdentifierOrPrivateName("Property", node);
             break;
         case "AssignmentPattern":
             if (node === parentNode.left && parentParentNode.type === "FunctionDeclaration") {
                 // function parameter with default value
-                logIdentifierOrPrivateName("Parameter", node);
+                parseData.logIdentifierOrPrivateName("Parameter", node);
             }
             break;
         case "AssignmentExpression":
             if (node === parentNode.left) {
-                logIdentifierOrPrivateName("AssignmentTarget", node);
+                parseData.logIdentifierOrPrivateName("AssignmentTarget", node);
             }
             break;
         case "ClassExpression":
-            logIdentifierOrPrivateName("Class", node);
+            parseData.logIdentifierOrPrivateName("Class", node);
             break;
     }
 }
@@ -157,7 +161,7 @@ function visitIdentifierOrPrivateName(path) {
  In particular, this redeclared variables from crashing the traversal
  when the AST was produced from parsing with errorRecovery: true.
  */
-function traverseAst(ast, disableScope) {
+function traverseAst(ast, parseData, disableScope) {
     /*
       TODO
        1. Consider adding state to allow distinction between elements from different arrays
@@ -166,92 +170,74 @@ function traverseAst(ast, disableScope) {
     const arrayVisitor = {
         noScope: disableScope,
         StringLiteral: function(path) {
-            const loc = locationString(path.node);
-            logLiteral("String", path.node.value, loc, true, path.node.extra);
+            const loc = position(path.node);
+            this.parseData.logLiteral("String", path.node.value, loc, true, path.node.extra);
         },
         NumericLiteral: function(path) {
-            const loc = locationString(path.node);
-            logLiteral("Numeric", path.node.value, loc, true, path.node.extra);
+            const loc = position(path.node);
+            this.parseData.logLiteral("Numeric", path.node.value, loc, true, path.node.extra);
         },
         BigIntLiteral: function(path) {
-            const loc = locationString(path.node);
-            logLiteral("Numeric", path.node.value, loc, true, path.node.extra);
+            const loc = position(path.node);
+            this.parseData.logLiteral("Numeric", path.node.value, loc, true, path.node.extra);
         },
         RegExpLiteral: function(path) {
-            const loc = locationString(path.node);
-            logLiteral("Regexp", path.node.pattern, loc, true, path.node.extra);
+            const loc = position(path.node);
+            this.parseData.logLiteral("Regexp", path.node.pattern, loc, true, path.node.extra);
         },
         TemplateElement: function(path) {
-            const loc = locationString(path.node);
-            logLiteral("StringTemplate", path.node.value.raw, loc, true, path.node.value);
+            const loc = position(path.node);
+            this.parseData.logLiteral("StringTemplate", path.node.value.raw, loc, true, path.node.value);
         }
     };
 
     const astVisitor = {
         noScope: disableScope,
-        Identifier: visitIdentifierOrPrivateName,
-        PrivateName: visitIdentifierOrPrivateName,
+        Identifier: function (path) {
+            visitIdentifierOrPrivateName(path, this.parseData);
+        },
+        PrivateName: function (path) {
+            visitIdentifierOrPrivateName(path, this.parseData);
+        },
         StringLiteral: function (path) {
-            const loc = locationString(path.node);
-            logLiteral("String", path.node.value, loc, false, path.node.extra);
+            const loc = position(path.node);
+            this.parseData.logLiteral("String", path.node.value, loc, false, path.node.extra);
         },
         DirectiveLiteral: function (path) {
             // same as string literal
-            const loc = locationString(path.node);
-            logLiteral("String", path.node.value, loc, false, path.node.extra);
+            const loc = position(path.node);
+            this.parseData.logLiteral("String", path.node.value, loc, false, path.node.extra);
         },
         NumericLiteral: function (path) {
-            const loc = locationString(path.node);
-            logLiteral("Numeric", path.node.value, loc, false, path.node.extra);
+            const loc = position(path.node);
+            this.parseData.logLiteral("Numeric", path.node.value, loc, false, path.node.extra);
         },
         BigIntLiteral: function(path) {
-            const loc = locationString(path.node);
-            logLiteral("Numeric", path.node.value, loc, false, path.node.extra);
+            const loc = position(path.node);
+            this.parseData.logLiteral("Numeric", path.node.value, loc, false, path.node.extra);
         },
         RegExpLiteral: function(path) {
-            const loc = locationString(path.node);
-            logLiteral("Regexp", path.node.pattern, loc, false, path.node.extra);
+            const loc = position(path.node);
+            this.parseData.logLiteral("Regexp", path.node.pattern, loc, false, path.node.extra);
         },
         ArrayExpression: function (path) {
-            path.traverse(arrayVisitor);
+            path.traverse(arrayVisitor, { parseData });
             path.skip();
         },
         TemplateElement: function (path) {
-            const loc = locationString(path.node);
-            logLiteral("StringTemplate", path.node.value.raw, loc, false, path.node.value);
+            const loc = position(path.node);
+            this.parseData.logLiteral("StringTemplate", path.node.value.raw, loc, false, path.node.value);
         }
     };
 
-    traverse(ast, astVisitor);
+    traverse(ast, astVisitor, null, { parseData });
 }
 
-function main() {
-    /*
-     If false, syntax errors result in immediate termination of the program with
-     SYNTAX_ERROR_EXIT_CODE, and JSON output will be suppressed.
-     If true, the parser will recover from minor syntax errors where possible, and
-     record error details in the output JSON. If not possible to recover, the program
-     will still terminate with SYNTAX_ERROR_EXIT_CODE.
-     */
-    let allowSyntaxErrors = false;
+function parseFile(fileName, allowSyntaxErrors, includeAST) {
+    const sourceCode = fs.readFileSync(fileName, "utf8");
 
-    let printDebug = false;
-
-    /*
-     Referencing process.stdin.fd (actually just process.stdin) causes stdin to become nonblocking
-     Therefore running this in a terminal in interactive mode with no file piped into stdin will
-     cause the read to fail with EAGAIN. Passing the raw '0' as the fd avoids this issue.
-     See https://github.com/nodejs/help/issues/2663
-     */
-    const sourceFile = process.argv.length >= 3 ? process.argv[2] : 0;
-    if (process.argv[process.argv.length - 1] === "debug") {
-        printDebug = true;
-    }
-
-    const sourceCode = fs.readFileSync(sourceFile, "utf8");
-    logInfo("InputLength", sourceCode.length);
-
-    let unrecoverableSyntaxError = false;
+    const parseData = new ParseData();
+    parseData.logInfo("InputLength", sourceCode.length.toString());
 
     try {
         const ast = parser.parse(sourceCode, {
@@ -259,43 +245,62 @@ function main() {
             sourceType: "unambiguous" // parser is allowed to parse input as either script or module
         });
 
-        if (printDebug) {
-            console.log("AST");
-            console.log(JSON.stringify(ast, null, "  "));
+        if (includeAST) {
+            parseData.ast = ast;
         }
 
         for (let e of ast.errors) {
             let pos = `[${e.loc.line},${e.loc.column}]`;
-            logError(e.name, `${e.code}: ${e.reasonCode}`, pos);
+            parseData.logError(e.name, `${e.code}: ${e.reasonCode}`, pos);
         }
 
         for (let c of ast.comments) {
-            let loc = locationString(c);
-            logComment(c.type, c.value, loc);
+            let loc = position(c);
+            parseData.logComment(c.type, c.value, loc);
         }
 
-        traverseAst(ast, allowSyntaxErrors);
+        traverseAst(ast, parseData, allowSyntaxErrors);
 
     } catch (e) {
         if (e instanceof SyntaxError) {
-            unrecoverableSyntaxError = true;
-            let pos = `[${e.loc.line},${e.loc.column}]`;
-            logError(e.name, `${e.code}: ${e.reasonCode}`, pos);
+            let pos = [e.loc.line, e.loc.column];
+            parseData.logError(e.name, `${e.code}: ${e.reasonCode}`, pos);
+            parseData.logError(e.name, `${fatalSyntaxErrorMarker} (unable to parse remainder of file)`, pos);
         } else {
             throw(e);
         }
     }
 
-    const allJSON = "[\n" + parseOutputLines.join(",\n") + "\n]";
+    return parseData;
+}
 
-    const suppressJSON = !allowSyntaxErrors && unrecoverableSyntaxError;
-    if (!suppressJSON) {
-        console.log(allJSON);
+function main() {
+    /*
+     If allowSyntaxErrors is false, any syntax error results in immediate termination
+     of parsing for a file. If true, the parser will recover from minor syntax errors
+     where possible. All error details are recorded in the returned parseData object.
+     */
+    let allowSyntaxErrors = false;
+
+    let printDebug = false;
+
+    /*
+     Use stdin if no filename is specified.
+
+     Referencing process.stdin.fd (actually just process.stdin) causes stdin to become nonblocking
+     Therefore running this in a terminal in interactive mode with no file piped into stdin will
+     cause the read to fail with EAGAIN. Passing the raw '0' as the fd avoids this issue.
+     See https://github.com/nodejs/help/issues/2663
+     */
+    const sourceFile = process.argv.length >= 3 ? process.argv[2] : 0;
+
+    if (process.argv[process.argv.length - 1] === "debug") {
+        printDebug = true;
     }
 
-    if (unrecoverableSyntaxError) {
-        process.exit(SYNTAX_ERROR_EXIT_CODE);
-    }
+    const parseData = parseFile(sourceFile, allowSyntaxErrors, printDebug);
+
+    console.log(JSON.stringify(parseData, null, "  "));
 }
 
 main();
