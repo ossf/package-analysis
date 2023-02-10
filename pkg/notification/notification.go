6104b31396cea65727421225787df98660a10fa2
Ada Luong <adaluong@google.com>
2023-02-10 01:07:38 +0000

    Add function to parse analysis complete notification JSON
    Signed-off-by: Ada Luong <adaluong@google.com>

--- pkg/notification/notification.go
+++ pkg/notification/notification.go
@@ -1,6 +1,10 @@
 package notification
 
 import (
+	"encoding/json"
+	"fmt"
+	"gocloud.dev/pubsub"
+
 	"github.com/ossf/package-analysis/pkg/pkgidentifier"
 )
 
@@ -8,4 +12,13 @@ import (
 // package analysis completion.
 type AnalysisCompletion struct {
 	Package	pkgidentifier.PkgIdentifier
+}
+
+// ParseJSON takes in a notification JSON and returns an AnalysisCompletion struct.  
+func ParseJSON(msg *pubsub.Message) (AnalysisCompletion, error) {
+	notification := AnalysisCompletion{}
+	if err := json.Unmarshal(msg.Body, &notification); err != nil {
+		return notification, fmt.Errorf("error unmarshalling json: %w", err)
+	}
+	return notification, nil
 }
