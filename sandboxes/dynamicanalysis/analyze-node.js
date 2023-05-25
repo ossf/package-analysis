e5cecb7b8d8195579ac3f05b476c1ce34e7bff34
Max Fisher <maxfisher@google.com>
2023-05-25 21:58:03 +1000

    node, pypi: execute package code during import phase
    Signed-off-by: Max Fisher <maxfisher@google.com>

--- sandboxes/dynamicanalysis/analyze-node.js
+++ sandboxes/dynamicanalysis/analyze-node.js
@@ -1,23 +1,24 @@
 #!/usr/bin/env node
 
-const { spawnSync } = require('child_process');
-const { argv } = require('process');
+// eslint no-var: 0
+// jshint esversion: 6
+// jshint node: true
+"use strict";
 
-function install(package) {
+const {spawnSync} = require('child_process');
+const {argv} = require('process');
+
+function install(pkg) {
   // Specify the package to install.
-  const installPkg = package.localFile
-    ? package.localFile
-    : (package.version
-      ? `${package.name}@${package.version}`
-      : package.name);
+  const installPkg = pkg.localFile ? pkg.localFile : (pkg.version ? `${pkg.name}@${pkg.version}` : pkg.name);
 
-  let result = spawnSync('npm', ['init', '--force'], { stdio: 'inherit' });
-  if (result.status != 0) {
+  let result = spawnSync('npm', ['init', '--force'], {stdio: 'inherit'});
+  if (result.status !== 0) {
     throw 'Failed to init npm';
   }
 
-  result = spawnSync('npm', ['install', installPkg], { stdio: 'inherit' });
-  if (result.status == 0) {
+  result = spawnSync('npm', ['install', installPkg], {stdio: 'inherit'});
+  if (result.status === 0) {
     console.log('Install succeeded.');
   } else {
     // Always exit on failure.
@@ -28,12 +29,61 @@ function install(package) {
   }
 }
 
-function importPkg(package) {
+function importPkg(pkg) {
   try {
-    require(package.name);
+    const mod = require(pkg.name);
+    useModule(mod);
   } catch (e) {
-    console.log(`Failed to import ${package.name}: ${e}`);
-  }  
+    console.log(`Failed to import ${pkg.name}: ${e}`);
+  }
+}
+
+// Adapted from https://stackoverflow.com/a/72326559.
+// Premise is that classes have a non-writable prototype
+function isES6Class(obj) {
+  if (typeof obj !== "function") {
+    return false;
+  }
+
+  const descriptor = Object.getOwnPropertyDescriptor(obj, "prototype");
+
+  return descriptor && !descriptor.writable;
+}
+
+// best-effort execution of code in the module
+function useModule(modulePath) {
+  console.log("[module] " + modulePath);
+
+  //console.log(module);
+
+  // How to tell if something is a function vs class:
+  // - node uses complex internal logic in util.inspect() function [1]
+  // - StackOverflow thread discussing how complex this problem is [2]
+  //
+  // Solution uses code from an answer in [2]
+
+  // [1] https://github.com/nodejs/node/blob/main/lib/internal/util/inspect.js
+  // [2] https://stackoverflow.com/questions/30758961/how-to-check-if-a-variable-is-an-es6-class-declaration/72326559
+  const callableNames = Object.keys(module).filter(
+    (key) => typeof module[key] === "function"
+  );
+
+  // Call all the exported names. If there is a
+  // TypeError, it's probably because it is a class,
+  // so we'll try again using new
+  for (const name of callableNames) {
+    try {
+      if (isES6Class(module[name])) {
+        console.log("[class] " + name);
+        const instance = new module[name]();
+      } else {
+        console.log("[function] " + name);
+        module[name]();
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
 }
 
 const phases = new Map([
@@ -66,9 +116,9 @@ switch (argv[0]) {
 }
 
 const phase = argv.shift();
-const pkg = argv.shift();
-const package = {
-  name: pkg,
+const pkgName = argv.shift();
+const pkg = {
+  name: pkgName,
   version: ver,
   localFile: localFile,
 };
@@ -79,4 +129,4 @@ if (!Array.from(phases.keys()).includes(phase)) {
 }
 
 // Execute the phase
-phases.get(phase).forEach((f) => f(package));
+phases.get(phase).forEach((f) => f(pkg));
--- sandboxes/dynamicanalysis/analyze-python.py
+++ sandboxes/dynamicanalysis/analyze-python.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import inspect
 from dataclasses import dataclass
 import importlib
 from importlib.metadata import files
@@ -6,8 +7,10 @@ import sys
 import subprocess
 import traceback
 from typing import Optional
+from unittest.mock import MagicMock
 
 PY_EXTENSION = '.py'
+
 
 @dataclass
 class Package:
@@ -24,22 +27,24 @@ class Package:
         else:
             return self.name
 
+
 def install(package):
     """Pip install."""
     arg = package.install_arg()
     try:
-      output = subprocess.check_output(
-          (sys.executable, '-m', 'pip', 'install', '--pre', arg),
-          stderr=subprocess.STDOUT)
-      print('Install succeeded:')
-      print(output.decode())
+        output = subprocess.check_output(
+            (sys.executable, '-m', 'pip', 'install', '--pre', arg),
+            stderr=subprocess.STDOUT)
+        print('Install succeeded:')
+        print(output.decode())
     except subprocess.CalledProcessError as e:
-      print('Failed to install:')
-      print(e.output.decode())
-      # Always raise.
-      # Install failing is either an interesting issue, or an opportunity to
-      # improve the analysis.
-      raise
+        print('Failed to install:')
+        print(e.output.decode())
+        # Always raise.
+        # Install failing is either an interesting issue, or an opportunity to
+        # improve the analysis.
+        raise
+
 
 def path_to_import(path):
     """Convert a path to import."""
@@ -61,10 +66,80 @@ def importPkg(package):
         import_path = path_to_import(path)
         print('Importing', import_path)
         try:
-            importlib.import_module(import_path)
+            module = importlib.import_module(import_path)
+            execute_module(module)
         except:
             print('Failed to import', import_path)
             traceback.print_exc()
+
+
+def invoke_function(obj):
+    signature = inspect.signature(obj)
+    args = []
+    kwargs = {}
+
+    for name, param in signature.parameters.items():
+        # use MagicMock to create semi-realistic function argument values
+        # https://docs.python.org/3/library/unittest.mock.html
+        value = MagicMock() if param.default == param.empty else param.default
+
+        match param.kind:
+            case param.POSITIONAL_ONLY:
+                args.append(value)
+            case param.KEYWORD_ONLY | param.POSITIONAL_OR_KEYWORD:
+                kwargs[name] = value
+            case param.VAR_POSITIONAL:  # when *args appears in signature
+                pass  # ignore
+            case param.VAR_KEYWORD:  # when **args appears in signature
+                pass  # ignore
+
+    # bind args and invoke the function
+    # any exceptions will be propagated to the caller
+    bound = signature.bind(*args, **kwargs)
+    return obj(*bound.args, **bound.kwargs)
+
+
+def try_invoke_function(name, obj, is_method=False):
+    tag = "[method]" if is_method else "[function]"
+    print(tag, name)
+
+    try:
+        ret = invoke_function(obj)
+        print("[return value]", ret)
+    except Exception as e:
+        print(type(e), e, sep=": ")
+
+
+def try_instantiate_class(name, obj):
+    print("[class]", name)
+
+    def is_non_init_method(m):
+        return inspect.ismethod(m) and m.__name__ != "__init__"
+
+    try:
+        instance = invoke_function(obj)
+        methods = inspect.getmembers(instance, is_non_init_method)
+        for name, method in methods:
+            try_invoke_function(name, method, is_method=True)
+    except Exception as e:
+        print(type(e), e, sep=": ")
+
+
+def execute_module(module):
+    """Best-effort execution of code in a module"""
+    print("[module]", module)
+
+    skipped_names = []
+    for (name, module_member) in inspect.getmembers(module):
+        if inspect.isfunction(module_member):
+            try_invoke_function(name, module_member)
+        elif inspect.isclass(module_member):
+            try_instantiate_class(name, module_member)
+        else:
+            skipped_names.append(name)
+
+    print("[skipped members]", " ".join(skipped_names))
+
 
 PHASES = {
     "all": [install, importPkg],
