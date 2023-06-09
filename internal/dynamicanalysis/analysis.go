60d9b729c6a4ba42a9fb28dc924c23fcdea68050
Max Fisher <maxfisher@google.com>
2023-06-09 11:05:42 +1000

    lastNBytes: don't copy buffer when its length is equal to the limit
    Signed-off-by: Max Fisher <maxfisher@google.com>

--- internal/dynamicanalysis/analysis.go
+++ internal/dynamicanalysis/analysis.go
@@ -30,9 +30,9 @@ var resultError = &Result{
 }
 
 // lastNBytes returns the last n bytes from b.
-// If len(b) < n, b itself is returned, otherwise a copy of the bytes is returned.
+// If len(b) <= n, b itself is returned, otherwise a copy of the bytes is returned.
 func lastNBytes(b []byte, n int) []byte {
-	if len(b) < n {
+	if len(b) <= n {
 		return b
 	}
 	return b[(len(b) - n):]
