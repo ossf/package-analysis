3d5612039e798f89e1aa05523654f58c67fbee96
Elaine Chien <elainechien@chromium.org>
2023-07-07 20:38:15 +0000

    Start creating sample python package with https post request
    Signed-off-by: Elaine Chien <elainechien@chromium.org>

---
+++ sample_packages/sample_python_package/README.md
@@ -0,0 +1,3 @@
+## Sample Python package
+
+This package will simulate different scenarios to test package analysis on. 
---
+++ sample_packages/sample_python_package/src/example.py
@@ -0,0 +1,20 @@
+import http.client
+import json
+
+# Sends an HTTPS post request and prints out the response.
+def sendHTTPSPostRequest():
+  host = "www.httpbin.org"
+  conn = http.client.HTTPSConnection(host)
+  data = {'text': 'Sending data through HTTPS'}
+  json_data = json.dumps(data)
+  conn.request("POST", "/post", json_data, headers={"Host": host})
+  response = conn.getresponse()
+  print(response.read())
+
+
+def main():
+  sendHTTPSPostRequest()
+
+
+if __name__ == "__main__":
+  main()
