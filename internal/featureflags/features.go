e20030b2d32cc26bcdd872015001d2c3531967d6
Max Fisher <maxfisher@google.com>
2023-08-22 15:14:55 +1000

    disable code execution feature by default and tweak some comments
    Signed-off-by: Max Fisher <maxfisher@google.com>

--- internal/featureflags/features.go
+++ internal/featureflags/features.go
@@ -9,12 +9,12 @@ var (
 	// to the analyzed packages bucket (if configured) after analysis completes
 	SaveAnalyzedPackages = new("SaveAnalyzedPackages", false)
 
-	// PubSubExtender determines whether or not the worker uses a real GCP
-	// extender for keeping messages alive during long-running processing.
+	// PubSubExtender determines whether the worker uses a real GCP extender
+	// for keeping messages alive during long-running processing.
 	PubSubExtender = new("PubSubExtender", true)
 
-	// CodeExecution automatically invokes package code during the import phase
-	// of dynamic analysis, which may uncover extra malicious behaviour.
-	// A list executed function/method/class names is saved.
-	CodeExecution = new("CodeExecution", true)
+	// CodeExecution invokes package code automatically during dynamic analysis,
+	// which may uncover extra malicious behaviour. The names of executed functions,
+	// methods and classes are logged to a file.
+	CodeExecution = new("CodeExecution", false)
 )
