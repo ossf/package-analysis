8285aa556f3adcd5184d072191c28bdc79aabe67
Elaine Chien <elainechien@google.com>
2023-09-05 22:54:15 +0000

    Move try statement in access_ssh_keys function
    Signed-off-by: Elaine Chien <elainechien@google.com>

--- sample_packages/sample_python_package/src/example.py
+++ sample_packages/sample_python_package/src/example.py
@@ -20,27 +20,27 @@ def send_https_post_request(called_from: str, print_logs: bool) -> None:
 
 # Access ssh keys and attempts to read and write to them.
 def access_ssh_keys(called_from: str, print_logs: bool) -> None:
-  try:
     ssh_keys_directory_path = os.path.join(os.path.expanduser('~'), ".ssh")
     if os.path.isdir(ssh_keys_directory_path):
-      files_in_ssh_keys_directory = os.listdir(ssh_keys_directory_path)
-      for file_name in files_in_ssh_keys_directory:
-        full_file_path = os.path.join(ssh_keys_directory_path, file_name)
-        original_file_lines = []
-        with open(full_file_path, "a+") as f:
-          f.seek(0)
-          original_file_lines.extend(f.readlines())
-          f.write("\nWriting to files in ~/.ssh from: " + called_from)
-        # Reset the original state of the files.
-        with open(full_file_path, "w") as f:
-          for line in original_file_lines:
-            f.write(line)
-      if(print_logs):
-        print("Files in ssh keys directory", files_in_ssh_keys_directory)
-  except Exception as e:
-    # Fail gracefully to allow execution to continue.
-    if(print_logs):
-      print(f"An exception occurred when calling access_ssh_keys: {str(e)}")
+      try:
+        files_in_ssh_keys_directory = os.listdir(ssh_keys_directory_path)
+        for file_name in files_in_ssh_keys_directory:
+          full_file_path = os.path.join(ssh_keys_directory_path, file_name)
+          original_file_lines = []
+          with open(full_file_path, "a+") as f:
+            f.seek(0)
+            original_file_lines.extend(f.readlines())
+            f.write("\nWriting to files in ~/.ssh from: " + called_from)
+          # Reset the original state of the files.
+          with open(full_file_path, "w") as f:
+            for line in original_file_lines:
+              f.write(line)
+        if(print_logs):
+          print("Files in ssh keys directory", files_in_ssh_keys_directory)
+      except Exception as e:
+        # Fail gracefully to allow execution to continue.
+        if(print_logs):
+          print(f"An exception occurred when calling access_ssh_keys: {str(e)}")
 
 def read_file_and_log(file_to_read: str, called_from: str, print_logs: bool) -> None:
   if os.path.isfile(file_to_read):
