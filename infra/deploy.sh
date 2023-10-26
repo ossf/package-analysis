d36ac5dc73dbcac5c8bb0877d73265b644099b94
Max Fisher <maxfisher@google.com>
2023-10-26 13:54:38 +1100

    address review comments
    Signed-off-by: Max Fisher <maxfisher@google.com>

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -5,17 +5,24 @@ if [[ -z ${GIT_TAG} ]]; then
 	exit 1
 fi
 
+echo "git checkout ${GIT_TAG}"
 git checkout "${GIT_TAG}"
+
+if ! git diff-index --quiet HEAD; then
+	echo "there are uncommitted changes, please ensure the repo is clean"
+	exit 1
+fi
+
 gcloud container clusters get-credentials analysis-cluster --zone=us-central1-c --project=ossf-malware-analysis
 
-pushd infra/worker || echo "could not change to infra/worker directory" && exit 1
+pushd infra/worker || echo "pushd infra/worker failed" && exit 1
 
 echo "Were any changes made to the k8s config?"
 echo "Enter y to apply config changes and then restart workers, n to just restart, ctrl-C to exit"
-read yn
+read -r yn
 case $yn in
 	[Yy]* )
-		echo "kubectl apply -f ."
+		echo "kubectl apply -f $(pwd)"
 		kubectl apply -f .
 		;;
 	[Nn]* )
@@ -24,3 +31,5 @@ case $yn in
 	;;
 esac
 
+
+popd || echo "failed to popd" && exit 1
