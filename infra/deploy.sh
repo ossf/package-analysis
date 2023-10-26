21dd2c5d999106cd59175867a87b3aec5692e6f3
Max Fisher <maxfisher@google.com>
2023-10-26 11:53:26 +1100

    add script for kubernetes deployment
    Signed-off-by: Max Fisher <maxfisher@google.com>

---
+++ infra/deploy.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+if [[ -z ${GIT_TAG} ]]; then
+	echo "Missing git tag"
+	exit 1
+fi
+
+git checkout "${GIT_TAG}"
+gcloud container clusters get-credentials analysis-cluster --zone=us-central1-c --project=ossf-malware-analysis
+
+pushd infra/worker
+
+echo "Were any changes made to the k8s config?"
+echo "Enter y to apply config changes and then restart workers, n to just restart, ctrl-C to exit"
+read -p "" yn
+case $yn in
+	[Yy]* )
+		echo "kubectl apply -f ."
+		kubectl apply -f .
+		;;
+	[Nn]* )
+		echo "kubectl rollout restart statefulset workers-deployment"
+		kubectl rollout restart statefulset workers-deployment"
+	;;
+esac
+
