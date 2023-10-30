dc8695489a078603f662e75df0fa0dfbb02dfa0e
Max Fisher <maxfisher@google.com>
2023-10-30 12:35:13 +1100

    add static analysis loader function
    Signed-off-by: Max Fisher <maxfisher@google.com>

--- function/loader/load.go
+++ function/loader/load.go
@@ -55,3 +55,40 @@ func Load(ctx context.Context, m PubSubMessage) error {
 	log.Printf("Job created: %s", job.ID())
 	return nil
 }
+
+func LoadStaticAnalysis(ctx context.Context, m PubSubMessage) error {
+	project := os.Getenv("GCP_PROJECT")
+	bucket := os.Getenv("OSSF_MALWARE_STATIC_ANALYSIS_RESULTS")
+
+	bq, err := bigquery.NewClient(ctx, project)
+	if err != nil {
+		log.Panicf("Failed to create BigQuery client: %v", err)
+	}
+	defer bq.Close()
+
+	schema, err := bigquery.SchemaFromJSON(staticAnalysisSchemaJSON)
+	if err != nil {
+		log.Panicf("Failed to decode schema: %v", err)
+	}
+
+	gcsRef := bigquery.NewGCSReference(fmt.Sprintf("gs://%s/*.json", bucket))
+	gcsRef.Schema = schema
+	gcsRef.SourceFormat = bigquery.JSON
+	gcsRef.MaxBadRecords = 10000
+
+	dataset := bq.Dataset("packages")
+	loader := dataset.Table("staticanalysis").LoaderFrom(gcsRef)
+	loader.WriteDisposition = bigquery.WriteTruncate
+	loader.TimePartitioning = &bigquery.TimePartitioning{
+		Type:  bigquery.DayPartitioningType,
+		Field: "created",
+	}
+
+	job, err := loader.Run(ctx)
+	if err != nil {
+		log.Panicf("Failed to create load job: %v", err)
+	}
+
+	log.Printf("Job created: %s", job.ID())
+	return nil
+}
