45ed79ffd95a514a5b786e2325a0bba2b7730b0b
Tieg Zaharia <tieg.zaharia@gmail.com>
2025-02-09 14:09:02 -0700

    Test coverage: FloatEquals()
    Signed-off-by: Tieg Zaharia <tieg.zaharia@gmail.com>

---
+++ internal/utils/equals_test.go
@@ -0,0 +1,37 @@
+package utils
+
+import (
+	"math"
+	"testing"
+)
+
+func TestFloatEquals(t *testing.T) {
+	tests := []struct {
+		name     string
+		x1       float64
+		x2       float64
+		absTol   float64
+		expected bool
+	}{
+		{name: "equal", x1: 1.0, x2: 1.0, absTol: 0.0, expected: true},
+		{name: "gt tolerance", x1: 1.0, x2: 1.1, absTol: 0.1, expected: false},
+		{name: "lt 1 OoM tolerance", x1: 1.0, x2: 1.1, absTol: 0.2, expected: true},
+		{name: "lt 2 OoM tolerance", x1: 1.0, x2: 1.01, absTol: 0.02, expected: true},
+		{name: "NaN always equal", x1: math.NaN(), x2: math.NaN(), absTol: 0.123456789, expected: true},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			actual := FloatEquals(test.x1, test.x2, test.absTol)
+			if actual != test.expected {
+				t.Errorf(
+					"FloatEquals(%f, %f, %f) expected to be %t but was %t",
+					test.x1,
+					test.x2,
+					test.absTol,
+					test.expected,
+					actual,
+				)
+			}
+		})
+	}
+}
