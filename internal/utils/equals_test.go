1afad413e81404a9addf377a48408865c6c0230e
Tieg Zaharia <tieg.zaharia@gmail.com>
2025-02-09 14:50:31 -0700

    Test coverage: JSONEquals()
    Signed-off-by: Tieg Zaharia <tieg.zaharia@gmail.com>

--- internal/utils/equals_test.go
+++ internal/utils/equals_test.go
@@ -35,3 +35,74 @@ func TestFloatEquals(t *testing.T) {
 		})
 	}
 }
+
+func TestJSONEquals(t *testing.T) {
+	tests := []struct {
+		name     string
+		j1       []byte
+		j2       []byte
+		expected bool
+	}{
+		{
+			name:     "eq empty",
+			j1:       []byte("{}"),
+			j2:       []byte("{}"),
+			expected: true,
+		},
+		{
+			name:     "eq one key",
+			j1:       []byte(`{"a": 1}`),
+			j2:       []byte(`{"a": 1}`),
+			expected: true,
+		},
+		{
+			name:     "eq two keys",
+			j1:       []byte(`{"a": 1, "b": 1}`),
+			j2:       []byte(`{"a": 1, "b": 1}`),
+			expected: true,
+		},
+		{
+			name:     "eq two keys diff order",
+			j1:       []byte(`{"a": 1, "b": 1}`),
+			j2:       []byte(`{"b": 1, "a": 1}`),
+			expected: true,
+		},
+		{
+			name:     "deep eq",
+			j1:       []byte(`{"a": {"b": 1}}`),
+			j2:       []byte(`{"a": {"b": 1}}`),
+			expected: true,
+		},
+		{
+			name:     "deep eq diff order",
+			j1:       []byte(`{"a": {"b": 1, "c": 1}}`),
+			j2:       []byte(`{"a": {"c": 1, "b": 1}}`),
+			expected: true,
+		},
+		{
+			name:     "neq",
+			j1:       []byte(`{"a": 1}`),
+			j2:       []byte(`{"b": 1}`),
+			expected: false,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			actual, err := JSONEquals(test.j1, test.j2)
+
+			if err != nil {
+				t.Fatalf("JSONEquals() = %v; want no error", err)
+			}
+
+			if actual != test.expected {
+				t.Errorf(
+					"JSONEquals(%s, %s) expected to be %t but was %t",
+					test.j1,
+					test.j2,
+					test.expected,
+					actual,
+				)
+			}
+		})
+	}
+}
